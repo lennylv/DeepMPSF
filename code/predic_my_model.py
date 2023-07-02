12620c6bcf6fb5834747090fba6a3c854e1fa22e
Xie <534013577@qq.com>
2023-07-02 13:28:50 +0800

    upload

---
+++ code/predic_my_model.py
@@ -0,0 +1,34 @@
+import matplotlib
+import argparse
+
+import torch
+
+from Methods import split_shuffle_val_train_csvfile, get_test_data_csv
+from My_Model import My_Model_testing
+matplotlib.use('Agg')
+
+
+def main():
+    # ！！！需要修改变量的有5处：residues； Tablefile_train； Tablefile_test； name_onehot_file_train； name_onehot_file_test
+    predicttype = 'general'
+    mode = 'test'
+    residues = 'ST'
+    Tablefile_train = '../data/Table_All_train_general_ST.csv'
+    Tablefile_test = '../data/Table_All_test_general_ST.csv'
+
+    if predicttype == 'general':
+        if mode == 'blind_predict':
+            modelname = "general_model_{:s}".format(residues)
+            # 划分,shuffle： val 和train
+            # 生成已经 分割 且 shuffle 好的 Table_val_file 和 Table_train_file
+            shuffled_table_df_val, _, _ = split_shuffle_val_train_csvfile(Tablefile_train)
+        if mode == 'test':
+            modelname = 'general_model_{:s}'.format(residues)
+            shuffled_table_df_val, _, _ = split_shuffle_val_train_csvfile(Tablefile_train)
+            shuffled_table_df_test = get_test_data_csv(Tablefile_test)
+
+    My_Model_testing(shuffled_table_df_test, modelname, shuffled_table_df_val, torch.device('cuda:2'))
+
+if __name__ == "__main__":
+    main()
+
